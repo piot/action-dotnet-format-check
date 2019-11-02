95af2c0b0dbe669795be8227f5cf6edc5ed72918
Peter Bjorklund <piot@hotmail.com>
2019-11-02 11:43:41 +0100

    fix: use `dotnet-format` instead of `dotnet format`

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,13 @@ async function check() : Promise<number> {
   const workspace = 'src/'
 
   core.info(`Performing a dotnet format check on '${workspace}'`);
-  const execString = `dotnet format --check --dry-run --verbosity diag --workspace ${workspace}`
+  
+  // Dotnet format is now executed using the old format, since some builds report the following error:
+  //
+  // "Since you just installed the .NET Core SDK, you will need to logout or restart your session before running the tool you installed.
+  // You can invoke the tool using the following command: dotnet-format"
+
+  const execString = `dotnet-format --check --dry-run --verbosity diag --workspace ${workspace}`
 
   return exec.exec(execString)
 }
